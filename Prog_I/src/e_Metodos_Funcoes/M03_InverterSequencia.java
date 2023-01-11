85e9080fe349ee6629905c8964c6cac6ca82e8de
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-01-10 21:20:18 -0300

    Novos exercícios com vetor

--- Prog_I/src/e_Metodos_Funcoes/M03_InverterSequencia.java
+++ Prog_I/src/e_Metodos_Funcoes/M03_InverterSequencia.java
@@ -13,9 +13,30 @@ import java.util.Scanner;
 public class M03_InverterSequencia {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int vetor[] = {1,2,3,4,5,6,7,8,9,10};
-        //int vetor[] = new int[20];
-        
+        int vetor[] = new int[20];
 
+        for (int i = 0;i< vetor.length;i++){
+            vetor[i] = teclado.nextInt();
+        }
+
+        mostrarVetor(vetor);
+        System.out.println();
+
+        inverterVetor(vetor);
+        mostrarVetor(vetor);
+    }
+
+    public static void inverterVetor(int vetor[]) {
+        int aux;
+        for (int i = 0; i < (vetor.length / 2); i++) {
+            aux = vetor[i];
+            vetor[i] = vetor[vetor.length - i - 1];
+            vetor[vetor.length - i - 1] = aux;
+        }
+    }
+    public static void mostrarVetor(int vetor[]){
+        for( int v:vetor){
+            System.out.print("[" + v + "] ");
+        }
     }
 }
