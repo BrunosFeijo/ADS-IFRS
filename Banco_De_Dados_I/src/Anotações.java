1088556eaa50db6681e76ad7a01905a6cdad6267
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-03-21 21:06:35 -0300

    Banco de Dados

--- Banco_De_Dados_I/src/Anotações.java
+++ Banco_De_Dados_I/src/Anotações.java
@@ -1,5 +1,7 @@
 /*
+Entidade relacional: (Banco de Dados)
  1- Entidades: Objetos - Tudo o que for concreto, visível...
+    *Normalmente colocadas no plural
  2- Atributos: Características deste objeto
  3- Relações: Conexão entre os objetos
 */
