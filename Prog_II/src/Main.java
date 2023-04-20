597c005bf4c7c331db8ccc4ee8e73f2276f55fff
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-04-19 21:19:17 -0300

    Teste

--- Prog_II/src/Main.java
+++ Prog_II/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Bruno");
+        nomes.add("Camila");
+        nomes.add("Ana");
+
+        System.out.println(nomes);
+        System.out.println("Tamanho: " + nomes.size());
+
+        nomes.remove(1);
+
+        System.out.println(nomes);
+        System.out.println("Tamanho: " + nomes.size());
     }
 }
