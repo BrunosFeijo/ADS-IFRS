1bd59f951792c254b3738719f3e426901e276bc3
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-04-29 20:50:31 -0300

    Estrutura de Dados

--- Estrutura_De_Dados/src/Ex3MenuComPOO/Main.java
+++ Estrutura_De_Dados/src/Ex3MenuComPOO/Main.java
@@ -9,9 +9,21 @@ public class Main {
     public static int menu(){
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Preencher o vetor com números aleatórios");
-        System.out.println("Preencher o vetor com números aleatórios já ordenados");
-        System.out.println("Informar maior e menor valor do vetor");
+        System.out.println("1- Preencher o vetor com números aleatórios");
+        System.out.println("2- Preencher o vetor com números aleatórios já ordenados");
+        System.out.println("3- Informar a quantidade de elementos");
+        System.out.println("4- Informar o maior e o menor valor existentes");
+        System.out.println("5- Listar elementos");
+        System.out.println("6- Inserir valor(por posição)");
+        System.out.println("7- Apagar elemento (por valor)");
+        System.out.println("8- Apagar elemento (por posição)");
+        System.out.println("9- Consultar valor (busca sequencial)");
+        System.out.println("10- Consultar valor (busca binária)");
+        System.out.println("11- Ordenar vetor usando BubbleSort");
+        System.out.println("12- Ordenar vetor usando InsetionSort");
+        System.out.println("13- Ordenar vetor usando SelectionSort");
+        System.out.println("14- Ordenar vetor usando QuickSort");
+        System.out.println("15- Limpar vetor");
 
         return entrada.nextInt();
     }
--- Estrutura_De_Dados/src/Ex3MenuComPOO/Vetor.java
+++ Estrutura_De_Dados/src/Ex3MenuComPOO/Vetor.java
@@ -1,4 +1,43 @@
 package Ex3MenuComPOO;
 
 public class Vetor {
+    private int[] vetor;
+    private int tamanho;
+
+    Vetor(){
+        vetor = new int[10];
+        tamanho = 0;
+    }
+    Vetor(int qtd){
+        vetor = new int[qtd];
+        tamanho = 0;
+    }
+
+    public void preencherVetor(){
+
+    }
+    public void preencherVetorOrdenado(){
+        preencherVetor();
+
+    }
+    public void inserirValor(int valor){
+
+    }
+    public void inserirValor(int valor, int posicao){
+
+    }
+    public int informarValorNaPosicao(){
+
+        return 0;
+    }
+    public int getTamanho(){
+        return tamanho;
+    }
+    public int getTamanhoMaximo(){
+        return vetor.length;
+    }
+    
+
+
+
 }
