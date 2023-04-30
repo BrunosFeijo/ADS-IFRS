adaf6ea74f541cb24eacef6c6d06930525b70bf5
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-04-29 21:06:50 -0300

    Estrutura de Dados

--- Estrutura_De_Dados/src/Ex3MenuComPOO/Vetor.java
+++ Estrutura_De_Dados/src/Ex3MenuComPOO/Vetor.java
@@ -1,43 +1,108 @@
 package Ex3MenuComPOO;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Vetor {
     private int[] vetor;
     private int tamanho;
 
-    Vetor(){
+    Vetor() {
         vetor = new int[10];
         tamanho = 0;
     }
-    Vetor(int qtd){
+
+    Vetor(int qtd) {
         vetor = new int[qtd];
         tamanho = 0;
     }
 
-    public void preencherVetor(){
+    public boolean preencherVetor() {
+        Random valores = new Random();
+        boolean preechido = false;
 
+        for (int i = tamanho; i < vetor.length; i++) {
+            vetor[i] = valores.nextInt(100000) + 1;
+            preechido = true;
+        }
+
+        tamanho = vetor.length;
+
+        return preechido;
     }
-    public void preencherVetorOrdenado(){
+
+    public void preencherVetorOrdenado() {
         preencherVetor();
+        quickSort();
+    }
+
+    public void inserirValor(int valor) {
+        
+    }
+
+    public void inserirValor(int valor, int posicao) {
 
     }
-    public void inserirValor(int valor){
 
-    }
-    public void inserirValor(int valor, int posicao){
-
-    }
-    public int informarValorNaPosicao(){
+    public int informarValorNaPosicao() {
 
         return 0;
     }
-    public int getTamanho(){
+
+    public int getTamanho() {
         return tamanho;
     }
-    public int getTamanhoMaximo(){
+
+    public int getTamanhoMaximo() {
         return vetor.length;
     }
-    
+
+    public void removerValor(int valor) {
+
+    }
+
+    public void removerValorDaPosicao(int posicao) {
+
+    }
+
+    public int menorValor() {
+        return 0;
+    }
+
+    public int maiorValor() {
+        return 0;
+    }
+
+    public int buscaSequencial() {
+        return 0;
+    }
+
+    public int buscaBinaria() {
+        return 0;
+    }
+
+    public void bubbleSort() {
+
+    }
+
+    public void insertionSort() {
+
+    }
+
+    public void selectionSort() {
+
+    }
+
+    public void quickSort() {
+
+    }
+
+    @Override
+    public String toString() {
 
 
-
+        return "Vetor{" +
+                "vetor=" + Arrays.toString(vetor) +
+                '}';
+    }
 }
