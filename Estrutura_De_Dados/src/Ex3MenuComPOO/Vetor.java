05ba54e7a13edabf2a8881ac3519f67918885f02
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-05-01 19:51:06 -0300

    Estrutura de Dados

--- Estrutura_De_Dados/src/Ex3MenuComPOO/Vetor.java
+++ Estrutura_De_Dados/src/Ex3MenuComPOO/Vetor.java
@@ -6,51 +6,70 @@ import java.util.Random;
 public class Vetor {
     private int[] vetor;
     private int tamanho;
-    private boolean cheio = false;
+    private boolean cheio;
 
     Vetor() {
         vetor = new int[10];
         tamanho = 0;
+        cheio = false;
     }
 
     Vetor(int qtd) {
         vetor = new int[qtd];
         tamanho = 0;
+        cheio = false;
     }
 
     public boolean preencherVetor() {
         Random valores = new Random();
 
-        if (!cheio){
+        if (!cheio) {
             for (int i = tamanho; i < vetor.length; i++) {
                 vetor[i] = valores.nextInt(100000) + 1;
             }
             tamanho = vetor.length;
-            return cheio = true;
-        }else {
+            return (cheio = true);
+        } else {
             System.out.println("Vetor já está cheio");
             return false;
         }
     }
 
     public boolean preencherVetorOrdenado() {
-        preencherVetor();
-        quickSort();
-
-        return cheio;
+        if (preencherVetor()) {
+            quickSort();
+            return true;
+        }
+        return false;
     }
 
-    public void inserirValor(int valor) {
-
+    public boolean inserirValor(int valor) {
+        if (!cheio) {
+            vetor[tamanho++] = valor;
+            return true;
+        }
+        return false;
     }
 
-    public void inserirValor(int valor, int posicao) {
+    public boolean inserirValor(int valor, int posicao) {
+        if (posicao >= tamanho || posicao < 0) {
+            System.out.println("A posição é menor que 0 ou maior que o tamanho atual do vetor!");
+            return false;
+        }
+        int valorAtual = vetor[posicao];
 
+        vetor[posicao] = valor;
+        System.out.println("A posição " + posicao + "foi alterada de " + valorAtual + " para " + valor);
+
+        return true;
     }
 
-    public int informarValorNaPosicao() {
-
-        return 0;
+    public int informarValorNaPosicao(int posicao) {
+        if (posicao >= tamanho || posicao < 0) {
+            System.out.println("A posição é menor que 0 ou maior que o tamanho atual do vetor!");
+            return -1;
+        }
+        return vetor[posicao];
     }
 
     public int getTamanho() {
@@ -61,15 +80,37 @@ public class Vetor {
         return vetor.length;
     }
 
-    public void removerValor(int valor) {
+    public boolean removerValor(int valor) {
+        int posicao = buscaBinaria(valor);
+        if (posicao == -1) {
+            System.out.println("Número não localizado!");
+            return false;
+        }
+        vetor[posicao] = 0;
 
+        for (int i = posicao; i < tamanho - 1; i++) {
+            vetor[i] = vetor[i + 1];
+        }
+        tamanho--;
+        return true;
     }
 
-    public void removerValorDaPosicao(int posicao) {
+    public boolean removerValorDaPosicao(int posicao) {
+        if (posicao >= tamanho || posicao < 0) {
+            System.out.println("A posição é menor que 0 ou maior que o tamanho atual do vetor!");
+            return false;
+        }
+        vetor[posicao] = 0;
 
+        for (int i = posicao; i < tamanho - 1; i++) {
+            vetor[i] = vetor[i + 1];
+        }
+        tamanho--;
+        return true;
     }
 
     public int menorValor() {
+        
         return 0;
     }
 
@@ -81,7 +122,7 @@ public class Vetor {
         return 0;
     }
 
-    public int buscaBinaria() {
+    public int buscaBinaria(int valor) {
         return 0;
     }
 
