b5c6cc6319d48468baf56b328a7840b06a5d1bbe
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-05-03 06:51:49 -0300

    Prog II

--- Prog_II/src/Ex4_Livraria/Livraria.java
+++ Prog_II/src/Ex4_Livraria/Livraria.java
@@ -6,10 +6,11 @@ import java.util.Scanner;
 public class Livraria {
     private ArrayList<Livro> livros;
 
-    public Livraria(){
-        livros =new ArrayList<>();
+    public Livraria() {
+        livros = new ArrayList<>();
     }
-    public void cadastrarLivro(){
+
+    public void cadastrarLivro() {
         Scanner entrada = new Scanner(System.in);
 
         System.out.print("Informe o título do livro: ");
@@ -22,7 +23,7 @@ public class Livraria {
         String editora = entrada.nextLine();
 
         System.out.println("Informe o gênero literário do livro: ");
-        for (Categoria genero: Categoria.values()){
+        for (Categoria genero : Categoria.values()) {
             System.out.println(genero.ordinal() + " - " + genero.name());
         }
         Categoria categoria = Categoria.values()[entrada.nextInt()];
@@ -36,16 +37,43 @@ public class Livraria {
         System.out.print("Informe a quantidade em estoque do livro: ");
         int qtd = entrada.nextInt();
 
-        Livro livro = new Livro(titulo,codigo,editora,categoria,ano, valor, qtd);
+        Livro livro = new Livro(titulo, codigo, editora, categoria, ano, valor, qtd);
         livros.add(livro);
 
         System.out.println("Livro cadastrado com sucesso!\n");
     }
 
+    public int buscaQuantidadeEmEstoque(String nome) {
+        for (Livro livro : livros) {
+            if (nome.equalsIgnoreCase(livro.getTitulo())) return livro.getQtd();
+        }
+
+        return -1;
+    }
+
+    public String buscaPorValor(double valor) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Livro livro : livros) {
+            if (valor >= livro.getValor()) stringBuilder.append(livro).append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public String buscaPorCategoria(Categoria genero) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Livro livro : livros) {
+            if (genero.equals(livro.getCategoria())) stringBuilder.append(livro).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+    
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        for (Livro livro:livros){
+        for (Livro livro : livros) {
             stringBuilder.append(livro).append("\n\n");
         }
 
