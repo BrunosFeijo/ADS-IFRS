097bcacb5e9602cdbfd69bb4464b4c72914ca9a7
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-05-02 21:56:31 -0300

    Prog II

--- Prog_II/src/Ex4_Livraria/Livro.java
+++ Prog_II/src/Ex4_Livraria/Livro.java
@@ -5,11 +5,18 @@ public class Livro {
     private int codigo;
     private String editora;
     private Categoria categoria;
+    private int ano;
+    private double valor;
+    private int qtd;
 
-    public Livro(String titulo, int codigo, String editora, Categoria categoria) {
+    public Livro(String titulo, int codigo, String editora,
+                 Categoria categoria, int ano, double valor, int qtd) {
         this.titulo = titulo;
         this.codigo = codigo;
         this.editora = editora;
         this.categoria = categoria;
+        this.ano = ano;
+        this.valor = valor;
+        this.qtd = qtd;
     }
 }
