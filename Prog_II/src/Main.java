fa2ecbe981e22cee0695c86952a51dbe329f4e96
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-05-08 20:49:26 -0300

    Prog II

--- Prog_II/src/Main.java
+++ Prog_II/src/Main.java
@@ -3,12 +3,8 @@ import Ex4_Livraria.Livro;
 
 public class Main {
     public static void main(String[] args) {
-        for (Categoria categoria: Categoria.values()){
-            System.out.println(categoria.ordinal() + " - " + categoria);
-        }
-        int indice = 32;
-        Categoria categoria = Categoria.values()[indice];
-        System.out.println(categoria);
-        System.out.println(Categoria.values().length);
+        String teste = "TESTE";
+
+        System.out.println(teste);
     }
 }
