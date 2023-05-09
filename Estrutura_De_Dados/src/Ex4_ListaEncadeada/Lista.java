33e46b9593ba05630d4dda4643a72a6592ee3ad1
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-05-08 21:52:16 -0300

    Estrutura de Dados

--- Estrutura_De_Dados/src/Ex4_ListaEncadeada/Lista.java
+++ Estrutura_De_Dados/src/Ex4_ListaEncadeada/Lista.java
@@ -1,5 +1,25 @@
 package Ex4_ListaEncadeada;
 
 public class Lista {
+    protected No inicio;
+    protected No fim;
+    protected int tamanho;
 
+        public Lista(){
+            inicio = null;
+            fim = null;
+            tamanho = 0;
+        }
+        public boolean isVazio(){
+            return tamanho == 0;
+        }
+        protected void adicionarNoInicio(int valor){
+            No novoNo = new No(valor);
+            if (isVazio()){
+                inicio = fim = novoNo;
+            }else{
+                inicio = novoNo;
+                
+            }
+        }
 }
