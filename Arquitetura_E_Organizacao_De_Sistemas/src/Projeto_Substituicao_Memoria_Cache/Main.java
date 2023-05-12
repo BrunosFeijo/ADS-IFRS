9a82210387c4b63053bb21c3ff4ae413803bd290
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-05-12 20:45:13 -0300

    Testes

--- Arquitetura_E_Organizacao_De_Sistemas/src/Projeto_Substituicao_Memoria_Cache/Main.java
+++ Arquitetura_E_Organizacao_De_Sistemas/src/Projeto_Substituicao_Memoria_Cache/Main.java
@@ -1,22 +1,26 @@
 package Projeto_Substituicao_Memoria_Cache;
 
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            FileInputStream arquivo = new FileInputStream("src/teste.txt");
-            Scanner entrada = new Scanner(arquivo);
-            String vetor[];
-            String linha;
-            while (entrada.hasNextLine()) {
-                linha = entrada.nextLine();
+            File arquivo = new File("src/Projeto_Substituicao_Memoria_Cache/teste.txt");
+            Path pasta = arquivo.toPath();
+            String[] vetor;
+            String linha = Files.readString(pasta);
+            while (linha != null) {
                 System.out.println(linha);
                 vetor = linha.split(",");
+                linha = Files.readString(pasta);
             }
         } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
