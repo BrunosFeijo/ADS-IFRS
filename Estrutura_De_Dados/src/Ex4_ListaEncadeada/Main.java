db560173ec7716586a464bf7fc4f19535d3ca30e
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-05-15 06:59:03 -0300

    Lista Encadeada

--- Estrutura_De_Dados/src/Ex4_ListaEncadeada/Main.java
+++ Estrutura_De_Dados/src/Ex4_ListaEncadeada/Main.java
@@ -3,11 +3,14 @@ package Ex4_ListaEncadeada;
 public class Main {
     public static void main(String[] args) {
         Lista lista = new Lista();
-        lista.adicionarNoInicio(1);
-        lista.adicionarNoInicio(2);
-        lista.adicionarNoInicio(3);
-        lista.adicionaNoFinal(2);
-        lista.adicionaNoFinal(3);
+        lista.adicionaEmOrdemCrescente(2);
+        lista.adicionaEmOrdemCrescente(5);
+        lista.adicionaEmOrdemCrescente(1);
+        lista.adicionaEmOrdemCrescente(3);
+        lista.adicionaEmOrdemCrescente(4);
+        lista.adicionaEmOrdemCrescente(-10);
+        lista.adicionaEmOrdemCrescente(100);
+        lista.adicionaEmOrdemCrescente(99);
         System.out.println(lista);
         System.out.println(lista.getTamanho());
     }
