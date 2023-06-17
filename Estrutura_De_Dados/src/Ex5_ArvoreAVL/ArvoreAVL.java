5ecee73919f347ac06848d7801ce541f8e246e68
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-06-17 19:09:56 -0300

    ÁrvoreAVL

--- Estrutura_De_Dados/src/Ex5_ArvoreAVL/ArvoreAVL.java
+++ Estrutura_De_Dados/src/Ex5_ArvoreAVL/ArvoreAVL.java
@@ -4,60 +4,69 @@ public class ArvoreAVL {
     private No raiz;
 
     public ArvoreAVL() {
+        raiz = null;
     }
-    public void deletarArvore(){
+
+    public void limparArvore() {
 
     }
-    public No getRaiz(){
+
+    public No getRaiz() {
         return raiz;
     }
-    public boolean isVazio(){
+
+    public boolean isVazio() {
         return raiz == null;
     }
-    public void inserir(int valor){
 
+    public void adicionar(int valor) {
+        boolean cresceu = false;
+        adicionarVerificandoBalanceamento(valor, raiz, cresceu);
     }
-    public void remover(int valor){
 
+    public void remover(int valor) {
     }
-    public void buscaRemover(int valor, No noAtual, boolean diminuiu){
 
+    public void buscaRemover(int valor, No noAtual, boolean diminuiu) {
     }
-    public void deletarNo(No noAtual, boolean isDiminuiu){
 
+    public void deletarNo(No noAtual, boolean isDiminuiu) {
     }
-    public void getFilho(int valor, No aux){
 
+    public void getFilho(int valor, No aux) {
     }
-    public boolean buscar(int valor){
+
+    public boolean buscar(int valor) {
         return true;
     }
-    public String imprimirPreOrdem(){
+
+    public String imprimirPreOrdem() {
         return null;
     }
-    public String imprimirPosOrdem(){
+
+    public String imprimirPosOrdem() {
         return null;
     }
-    public String imprimirEmOrdem(){
+
+    public String imprimirEmOrdem() {
         return null;
     }
-    private void rotacaoDireita(No noAtual){
 
-    }
-    private void rotacaoEsquerda(No noAtual){
-
-    }
-    private void rotacaoDireitaEsquerda(No noAtual){
-
-    }
-    private void rotacaoEsquerdaDireita(No noAtual){
-
-    }
-    private void defineRotacao(No noAtual){
-
-    }
-    private void inserirVerificandoBalanceamento(int valor, No noAtual, boolean isCresceu){
-
+    private void rotacaoDireita(No noAtual) {
     }
 
+    private void rotacaoEsquerda(No noAtual) {
+    }
+
+    private void rotacaoDireitaEsquerda(No noAtual) {
+    }
+
+    private void rotacaoEsquerdaDireita(No noAtual) {
+    }
+
+    private void defineRotacao(No noAtual) {
+    }
+
+    private void adicionarVerificandoBalanceamento(int valor, No noAtual, boolean isCresceu) {
+    }
 }
