cb9f4f130386c0ac5822055fc506ecf9efdee39b
Bruno Feijó <116765378+BrunosFeijo@users.noreply.github.com>
2023-11-01 01:20:09 -0300

    form

---
+++ Prog_III/Ex_IV_Formulários/scripts/script.js
@@ -0,0 +1,18 @@
+function validarFormulario(){
+    let cnpj = document.getElementById("cnpj");
+    let telefone = document.getElementById("telefone");
+
+    //Verificar CNPJ
+    let cnpjRegEx = /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/;
+    if(!cnpjRegEx.test(cnpj.value)){
+        alert('O CNPJ deve estar no formato XX.XXX.XXX/XXXX-XX');
+        return false;
+    }
+    // Verificação do telefone de contato
+    let telefoneRegex = /^\(\d{2}\)\d{5}-\d{4}$/;
+    if (!telefoneRegex.test(telefone.value)) {
+        alert('O telefone de contato deve estar no formato (XX)XXXXX-XXXX');
+        return false;
+    }
+    return true;
+}
